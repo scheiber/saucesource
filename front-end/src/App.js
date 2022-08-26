d6488227e1ac6c8edf2f86aeaebeda04f29b0025
Jonathan Scheiber <jon@scheiber.me>
2022-08-26 03:41:49 -0400

    update app.js, index.js, and package.json

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,11 +2,11 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-// import Index from "./pages/Index";
+import Index from "./pages/Index";
 import About from "./pages/About";
 import Home from "./pages/Home";
-// import New from "./pages/New";
-// import Show from "./pages/Show";
+import New from "./pages/New";
+import Show from "./pages/Show";
 // import Edit from "./pages/Edit";
 import Error from "./pages/Error";
 
@@ -18,10 +18,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        {/* <Route path="/sauces" element={<Index />} />
+        <Route path="/sauces" element={<Index />} />
         <Route path="/sauces/new" element={<New />} />
-        <Route path="/sauces/:index" element={<Show />} />
-        <Route path="/sauces/:index/edit" element={<Edit />} /> */}
+        <Route path="/sauces/:id" element={<Show />} />
+        {/* <Route path="/sauces/:index/edit" element={<Edit />} /> */}
         <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
