ad237ea86dce7a58abe3474f13db5da0612994f1
Jonathan Scheiber <jon@scheiber.me>
2022-08-26 18:59:39 -0400

    add jsdoc to scovilleFlames

--- front-end/src/helpers/scovilleFlames.js
+++ front-end/src/helpers/scovilleFlames.js
@@ -1,3 +1,9 @@
+/**
+ * Accepts a scoville heat unit (SHU) rating and returns a string with a specific amount of flame emoji characters. The higher the rating, the more characters are returned.
+ * @param  {Number} scoville A scoville heat unit (SHU) rating
+ * @return {string} A sequence of flame emoji characters
+ */
+
 const scovilleFlames = (scoville) => {
   return scoville > 20_000
     ? "🔥🔥🔥🔥🔥"
