0d0a4f901329bc4e22132ef39ff481ed4ec1b2b6
Jonathan Scheiber <jon@scheiber.me>
2022-08-31 16:09:56 -0400

    remove react-ga

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,5 @@
 import "./App.css";
-import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import ReactGA from "react-ga";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Index from "./pages/Index";
@@ -12,14 +10,7 @@ import Show from "./pages/Show";
 import Edit from "./pages/Edit";
 import Error from "./pages/Error";
 
-const TRACKING_ID = "UA-12916432-6";
-ReactGA.initialize(TRACKING_ID);
-
 function App() {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
-
   return (
     <div className="App">
       <NavBar />
