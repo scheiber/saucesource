e7989d3c94cb29e10291eecb4beafa800aaaec15
Jonathan Scheiber <jon@scheiber.me>
2022-08-31 15:02:12 -0400

    add react-ga

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import ReactGA from "react-ga";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Index from "./pages/Index";
@@ -10,7 +12,14 @@ import Show from "./pages/Show";
 import Edit from "./pages/Edit";
 import Error from "./pages/Error";
 
+const TRACKING_ID = "UA-12916432-6";
+ReactGA.initialize(TRACKING_ID);
+
 function App() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <div className="App">
       <NavBar />
