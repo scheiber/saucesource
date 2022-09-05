a37d635e0f2aa7b4e1bf9d191e3243fd8a66c803
Jonathan Scheiber <jon@scheiber.me>
2022-09-05 02:01:48 -0400

    correct indicator placement

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -19,6 +19,7 @@ const LoadingIndicator = (props) => {
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
+          paddingTop: "200px",
         }}
       >
         <RotatingLines
